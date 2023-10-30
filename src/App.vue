cd98d2249b56a71ae674af1380e5f276647b97e6
Abdullah-A-Alhabib <115307026+Abdullah-A-Alhabib@users.noreply.github.com>
2023-10-30 23:03:16 +0300

    Completed JS5
    I don't really know what part to upload so I uploaded this (srs).
    
    This took like 3-4 hours, 2-3 of which were just installing it. Teaching us about some random library or extension for only one class is really not beneficial in the slightest. Retweet if you like chicken.

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+    import { ref } from 'vue'
+    import imgUrl from '/src/chickn.png'
+
+    const count = ref(0)
+
+    const awesome = ref(false)
+
+</script>
+
+<template>
+    <button @click="awesome = !awesome">Toggle Chicken Visiblity</button>
+
+    <h1 v-if="awesome"><img :src="imgUrl" /></h1>
+    <h1 v-else></h1>
+</template>
